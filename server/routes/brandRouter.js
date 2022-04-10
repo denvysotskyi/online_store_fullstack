de2f5fe249fa0deb4023cd8deb1377feab7898b7
denvysotskyi <denvysotskyi@gmail.com>
2022-04-10 14:43:19 +0300

    Modified main routers

--- server/routes/brandRouter.js
+++ server/routes/brandRouter.js
@@ -1,8 +1,9 @@
 const Router = require('express')
 const router = new Router()
 const brandController = require('../controllers/brandController')
+const checkRole = require('../middleware/CheckRoleMiddleware')
 
 router.get('/', brandController.getAll)
-router.post('/', brandController.create)
+router.post('/', checkRole('ADMIN'), brandController.create)
 
 module.exports = router
