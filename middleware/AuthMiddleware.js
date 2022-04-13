4b28193164aeeb3333b35222fb27577a84af550f
denvysotskyi <denvysotskyi@gmail.com>
2022-04-13 11:34:50 +0300

    Modified a middlewares

--- middleware/AuthMiddleware.js
+++ middleware/AuthMiddleware.js
@@ -9,13 +9,13 @@ module.exports = function (err, req, res, next) {
     const token = req.headers.authorization.split(' ')[1]
 
     if (!token) {
-      return res.status(401).json({ message: `User haven't authorization` })
+      return res.status(401).json({ message: 'Пользователь не авторизован' })
     }
 
     const decoded = jwt.verify(token, process.env.SECRET_KEY)
     req.user = decoded
     next()
   } catch (e) {
-    return res.status(401).json({ message: `User haven't authorization` })
+    return res.status(401).json({ message: 'Пользователь не авторизован' })
   }
 }
--- middleware/CheckRoleMiddleware.js
+++ middleware/CheckRoleMiddleware.js
@@ -10,19 +10,19 @@ module.exports = function (role) {
       const token = req.headers.authorization.split(' ')[1]
 
       if (!token) {
-        return res.status(401).json({ message: `User haven't authorization` })
+        return res.status(401).json({ message: 'Пользователь не авторизован' })
       }
 
       const decoded = jwt.verify(token, process.env.SECRET_KEY)
 
       if (decoded.role !== role) {
-        return res.status(403).json({ message: `You haven't access` })
+        return res.status(403).json({ message: 'Вы не имеете прав доступа' })
       }
 
       req.user = decoded
       next()
     } catch (e) {
-      return res.status(401).json({ message: `User haven't authorization` })
+      return res.status(401).json({ message: 'Пользователь не авторизован' })
     }
   }
 }
